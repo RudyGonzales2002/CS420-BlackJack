f63191bbc9e84f1e01e6b3dfc01484bcaba29087
Rudy <gonzalesrudy2002@gmail.com>
2025-11-30 13:49:20 -0800

    Added Hand Class and Header

---
+++ Hand.cpp
@@ -0,0 +1,8 @@
+//
+// Created by rudy on 11/30/25.
+//
+
+#include "Hand.h"
+
+namespace hand {
+} // hand
---
+++ Hand.h
@@ -0,0 +1,13 @@
+//
+// Created by rudy on 11/30/25.
+//
+
+#ifndef CS420_BLACKJACK_HAND_H
+#define CS420_BLACKJACK_HAND_H
+
+namespace hand {
+    class Hand {
+    };
+} // hand
+
+#endif //CS420_BLACKJACK_HAND_H
